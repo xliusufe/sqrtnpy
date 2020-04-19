a34a5213e5e7dc750bbc7760831a4c7579e71c23
xliusufe <liuxuxm@163.com>
2020-04-19 22:59:06 +0800

    initial

--- build/lib.win-amd64-3.7/sqrtn/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : April 19, 2020
-# @Author  : Xu Liu
-# @Site    : 'https://github.com/xliusufe/factpy/'
-# @File    : __init__.py.p
-
-from .sqrtn import sqrtn
-__all__ = 'sqrtn'
